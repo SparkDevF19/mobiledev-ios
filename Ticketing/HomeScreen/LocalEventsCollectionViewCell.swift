7821f374a7c92c0d73c7f6e6cd647c5206d4ab74
Daniel Perez <Daniel Perez>
2019-10-19 17:09:41 -0400

    Home Screen worked on not displaying

---
+++ Ticketing/HomeScreen/LocalEventsCollectionViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  LocalEventsCollectionViewCell.swift
+//  Ticketing
+//
+//  Created by Daniel Perez on 10/15/19.
+//  Copyright © 2019 Cassandra Zuria. All rights reserved.
+//
+
+import UIKit
+
+class LocalEventsCollectionViewCell: UICollectionViewCell {
+
+    
+    
+    @IBOutlet weak var ImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
