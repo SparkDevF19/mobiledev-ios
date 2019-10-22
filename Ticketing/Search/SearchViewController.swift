678242f93c1a7f26f4d982974b3e49d7d867e55b
Cassandra Zuria <czuria1@gmail.com>
2019-10-22 11:32:15 -0400

    Add SearchViewController

---
+++ Ticketing/Search/SearchViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SearchViewController.swift
+//  Ticketing
+//
+//  Created by Cassandra Zuria on 10/22/19.
+//  Copyright © 2019 Cassandra Zuria. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
