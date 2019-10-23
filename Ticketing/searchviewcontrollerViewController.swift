39ff916ca6695f4e0902886d97e5ec4f9fc857a4
Reiner Gonzalez <rei61692@gmail.com>
2019-10-22 22:15:31 -0400

    added new viewcontroller file

---
+++ Ticketing/searchviewcontrollerViewController.swift
@@ -0,0 +1,30 @@
+//
+//  searchviewcontrollerViewController.swift
+//  Ticketing
+//
+//  Created by Reiner Gonzalez on 10/22/19.
+//  Copyright © 2019 Cassandra Zuria. All rights reserved.
+//
+
+import UIKit
+
+class searchviewcontrollerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
