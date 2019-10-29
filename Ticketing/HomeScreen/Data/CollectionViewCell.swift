e997d13afcf583aec059a14708f7aa6ccf86aaa8
Daniel Perez <Daniel Perez>
2019-10-29 11:22:09 -0400

    added progrtammaticaly stuff

--- Ticketing/HomeScreen/Data/CollectionViewCell.swift
+++ Ticketing/HomeScreen/Data/CollectionViewCell.swift
@@ -1,19 +1,17 @@
 //
-//  LocalEventsCollectionViewCell.swift
+//  CollectionViewCell.swift
 //  Ticketing
 //
-//  Created by Daniel Perez on 10/15/19.
+//  Created by Daniel Perez on 10/29/19.
 //  Copyright © 2019 Cassandra Zuria. All rights reserved.
 //
 
 import UIKit
 
-class LocalEventsCollectionViewCell: UICollectionViewCell {
+class CollectionViewCell: UICollectionViewCell {
 
-    
-    
-    @IBOutlet weak var ImageView: UIImageView!
-    
+    @IBOutlet weak var eventImage: UIImageView!
+
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
--- Ticketing/ViewController.swift
+++ Ticketing/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  Ticketing
 //
-//  Created by Cassandra Zuria on 9/24/19.
+//  Created by Daniel Perez on 10/29/19.
 //  Copyright © 2019 Cassandra Zuria. All rights reserved.
 //
 
@@ -10,12 +10,80 @@ import UIKit
 
 class ViewController: UIViewController {
     
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    var collectionView: UICollectionView!
+    
+    let horizontalLayout: UICollectionViewFlowLayout  = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        return layout
+    }()
+    
+    struct CellIdentifiers {
+        static let eventsCell = "CollectionViewCell"
     }
+    
+    struct CellNibNames {
+        static let eventsCell = "CollectionViewCell"
+    }
+    
+    let eventImages: [UIImage] = [
+        UIImage(named: "cat")!
+    ]
+   
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: horizontalLayout)
+        
+        view.addSubview(collectionView)
+        setupConstraints()
+        
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        
+        collectionView.setCollectionViewLayout(horizontalLayout, animated: true)
 
+        let eventsNib = UINib(nibName: CellNibNames.eventsCell, bundle: nil)
+        collectionView.register(eventsNib, forCellWithReuseIdentifier: CellIdentifiers.eventsCell)
+    }
+    
+    private func setupConstraints() {
+        
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.topAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+        
+    }
+    
 
+}
+
+extension ViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CellIdentifiers.eventsCell, for: indexPath) as! CollectionViewCell
+        cell.eventImage.image = UIImage(named: "cat")
+        return  cell
+    }
+}
+
+extension ViewController: UICollectionViewDelegateFlowLayout, UICollectionViewDelegate {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 50, height: 50)
+    }
+    
+    
+    
 }
 
