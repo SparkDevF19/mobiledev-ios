ef7e82d51dcc6c51dddc66ec6b066b386b9bc78f
Daniel Perez <Daniel Perez>
2019-10-31 11:24:58 -0400

    collection view layout is finished

--- Ticketing/ViewController.swift
+++ Ticketing/ViewController.swift
@@ -8,82 +8,189 @@
 
 import UIKit
 
+struct CustomData {
+       var title: String
+       var image: UIImage
+       var url: String
+   }
+   
 class ViewController: UIViewController {
     
-    var collectionView: UICollectionView!
+   // MARK: Data
     
-    let horizontalLayout: UICollectionViewFlowLayout  = {
+    let data = [
+        
+        CustomData(title: "Cat", image: #imageLiteral(resourceName: "cat"), url: "google.com/cat"  ),
+        CustomData(title: "Cat", image: #imageLiteral(resourceName: "cat"), url: "google.com/cat"  ),
+        CustomData(title: "Cat", image: #imageLiteral(resourceName: "cat"), url: "google.com/cat"  ),
+        CustomData(title: "Cat", image: #imageLiteral(resourceName: "cat"), url: "google.com/cat"  ),
+        CustomData(title: "Cat", image: #imageLiteral(resourceName: "cat"), url: "google.com/cat"  ),
+        CustomData(title: "Cat", image: #imageLiteral(resourceName: "cat"), url: "google.com/cat"  ),
+        CustomData(title: "Cat", image: #imageLiteral(resourceName: "cat"), url: "google.com/cat"  ),
+        CustomData(title: "Cat", image: #imageLiteral(resourceName: "cat"), url: "google.com/cat"  )
+    ]
+    // MARK: Layouts
+    
+    fileprivate let eventCollectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
         layout.scrollDirection = .horizontal
-        return layout
+        layout.minimumInteritemSpacing = 0
+        layout.minimumLineSpacing = 0
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        cv.translatesAutoresizingMaskIntoConstraints = false
+        cv.register(CustomCell.self, forCellWithReuseIdentifier: "cell")
+        cv.backgroundColor = .white
+        cv.isPagingEnabled = true
+        return cv
     }()
     
-    struct CellIdentifiers {
-        static let eventsCell = "CollectionViewCell"
-    }
+    fileprivate let upcomingView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        cv.translatesAutoresizingMaskIntoConstraints = false
+        cv.register(CustomCell.self, forCellWithReuseIdentifier: "cell")
+        cv.backgroundColor = .white
+        return cv
+    }()
     
-    struct CellNibNames {
-        static let eventsCell = "CollectionViewCell"
-    }
+    fileprivate let pastView: UICollectionView = {
+           let layout = UICollectionViewFlowLayout()
+           layout.scrollDirection = .horizontal
+           let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+           cv.translatesAutoresizingMaskIntoConstraints = false
+           cv.register(CustomCell.self, forCellWithReuseIdentifier: "cell")
+           cv.backgroundColor = .white
+           return cv
+       }()
     
-    let eventImages: [UIImage] = [
-        UIImage(named: "cat")!
-    ]
-   
+    // MARK: viewDidLoad
+    
     override func viewDidLoad() {
         
         super.viewDidLoad()
         
-        collectionView = UICollectionView(frame: .zero, collectionViewLayout: horizontalLayout)
+        eventCollectionView.delegate = self
+        eventCollectionView.dataSource = self
         
-        view.addSubview(collectionView)
-        setupConstraints()
+        upcomingView.delegate = self
+        upcomingView.dataSource = self
         
-        collectionView.dataSource = self
-        collectionView.delegate = self
+        pastView.delegate = self
+        pastView.dataSource = self
         
-        collectionView.setCollectionViewLayout(horizontalLayout, animated: true)
-
-        let eventsNib = UINib(nibName: CellNibNames.eventsCell, bundle: nil)
-        collectionView.register(eventsNib, forCellWithReuseIdentifier: CellIdentifiers.eventsCell)
+        setup()
+       
     }
     
-    private func setupConstraints() {
+// MARK: collectionView Setup
+    
+    func setup(){
         
+        view.addSubview(eventCollectionView)
+        view.addSubview(upcomingView)
+        view.addSubview(pastView)
+        
+        // These are just constraints for the collection view
+    
         NSLayoutConstraint.activate([
-            collectionView.topAnchor.constraint(equalTo: view.topAnchor),
-            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+            
+            eventCollectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            eventCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            eventCollectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            eventCollectionView.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.heightAnchor, multiplier: 0.3),
+            
+            
         ])
         
-    }
+        NSLayoutConstraint.activate([
+            
+            upcomingView.topAnchor.constraint(equalTo: eventCollectionView.bottomAnchor, constant: 0.3 ),
+            upcomingView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            upcomingView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            upcomingView.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.heightAnchor, multiplier: 0.4)
+            
+        ])
+        
+        NSLayoutConstraint.activate([
+            
+            pastView.topAnchor.constraint(equalTo: upcomingView.bottomAnchor, constant: 0.3 ),
+            pastView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            pastView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            pastView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: 0)
+            
+             
+        ])
+        
     
+    }
 
 }
 
-extension ViewController: UICollectionViewDataSource {
+// MARK: ViewController Delegates
+
+extension ViewController: UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
     
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        
+        
+        if collectionView == eventCollectionView{
+             return CGSize(width: collectionView.frame.width, height: collectionView.frame.width/2)
+        }
+         return CGSize(width: collectionView.frame.width/2, height: collectionView.frame.width/2)
+    }
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         
-        return 5
+        return data.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CellIdentifiers.eventsCell, for: indexPath) as! CollectionViewCell
-        cell.eventImage.image = UIImage(named: "cat")
-        return  cell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CustomCell
+        cell.data = self.data[indexPath.row]
+        return cell
     }
+    
 }
 
-extension ViewController: UICollectionViewDelegateFlowLayout, UICollectionViewDelegate {
+// MARK: CustomCell
+
+class CustomCell: UICollectionViewCell {
     
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: 50, height: 50)
+    var data: CustomData? {
+        didSet {
+            guard let data = data else {return}
+            bg.image = data.image
+        }
     }
     
+    fileprivate let bg: UIImageView = {
+       let iv = UIImageView()
+        iv.image = #imageLiteral(resourceName: "cat")
+        iv.translatesAutoresizingMaskIntoConstraints = false
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        iv.layer.cornerRadius = 12
+        return iv
+    }()
     
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        contentView.addSubview(bg)
+        bg.topAnchor.constraint(equalTo: contentView.topAnchor).isActive = true
+        bg.leadingAnchor.constraint(equalTo: contentView.leadingAnchor).isActive = true
+        bg.trailingAnchor.constraint(equalTo: contentView.trailingAnchor).isActive = true
+        bg.bottomAnchor.constraint(equalTo: contentView.bottomAnchor).isActive = true
+
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
 }
+
 
