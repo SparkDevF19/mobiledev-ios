2004c1e513f9e0f9e01e9f6358489ab48e78841d
Carlos Mendoza <1cmendoza1996@gmail.com>
2019-11-08 04:43:50 -0500

    Setting up FirebaseAPI Singleton and GoogleSignIn in AppDelegate

---
+++ Ticketing/Models/FirebaseAPI.swift
@@ -0,0 +1,20 @@
+//
+//  FirebaseAPI.swift
+//  Ticketing
+//
+//  Created by Carlos Mendoza on 11/8/19.
+//  Copyright © 2019 Cassandra Zuria. All rights reserved.
+//
+
+import Foundation
+import Firebase
+import GoogleSignIn
+
+class FirebaseAPI {
+    
+    static let shared = FirebaseAPI()
+    
+    private init(){}
+    
+
+}
