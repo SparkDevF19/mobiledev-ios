8670f6bcb89672e187e51ec9f0a5e02a7efc1681
Daniel C Perez <dcperez08@gmail.com>
2019-11-12 12:14:53 -0500

    add payment tableview cell and controller

---
+++ Ticketing/Payments/PaymentsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PaymentsViewController.swift
+//  Ticketing
+//
+//  Created by Daniel Perez on 11/12/19.
+//  Copyright © 2019 Cassandra Zuria. All rights reserved.
+//
+
+import UIKit
+
+class PaymentsViewController: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
