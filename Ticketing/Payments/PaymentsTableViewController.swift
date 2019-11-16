041a6258d1e576f19fabf9ea021d5a08a89bac02
Daniel C Perez <dcperez08@gmail.com>
2019-11-15 22:07:56 -0500

    add table and stack views w/ constaints

--- Ticketing/Payments/PaymentsTableViewController.swift
+++ Ticketing/Payments/PaymentsTableViewController.swift
@@ -1,8 +1,8 @@
 //
-//  PaymentsTableViewController.swift
+//  FavoritesViewController.swift
 //  Ticketing
 //
-//  Created by Daniel Perez on 11/12/19.
+//  Created by Daniel Perez on 10/10/19.
 //  Copyright © 2019 Cassandra Zuria. All rights reserved.
 //
 
@@ -10,81 +10,70 @@ import UIKit
 
 class PaymentsTableViewController: UITableViewController {
 
+    struct payment {
+          var name: String
+          var number: String
+          //var img: UIImage
+      }
+    
+    var favorites = [payment(name: "AMEX", number: "**** 1454",
+                     payment(name: "Visa", number: "**** 4738"),
+                     payment(name: "MasterCard", number: "**** 2383"),]
+    
+    
+    struct CellIdentifier {
+        static let PaymentsCell = "PaymentsTableViewCell"
+    }
+    
+    struct CellNibNames {
+        static let PaymentsCell = "PaymentsTableViewCell"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        let paymentsNib = UINib(nibName: CellIdentifier.PaymentsCell, bundle: nil)
+        tableView.register(paymentsNib, forCellReuseIdentifier: "PaymentsTableViewCell")
+        
     }
+    
+}
 
-    // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
+extension PaymentsViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return payments.count
     }
-
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PaymentsTableViewCell", for: indexPath) as! PaymentsTableViewCell
+        let newPayment = Payments[indexPath.row]
+        cell.paymentlabel.text = newPayment.name
+        cell.numberlabel.text = newPayment.number
+        cell.img.image = #imageLiteral(resourceName: "amex")
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        
+        
+        let deleteAction = UIContextualAction(style: .destructive, title: "Delete") { action, view, completionHandler in
+            
+            self.payments.remove(at: indexPath.row)
+            
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+            completionHandler(true)
+            
+        }
+        
+        deleteAction.backgroundColor = .red
+        
+        let configuration = UISwipeActionsConfiguration(actions: [deleteAction])
+        return configuration
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
+
+    
+  
+
