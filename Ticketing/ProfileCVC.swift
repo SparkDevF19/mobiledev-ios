1c7b1bbc921bba8f1f373a2e7cc00db63d805f31
JoseBello94 <josecarlosbp1229@gmail.com>
2019-11-18 12:05:48 -0500

    finished save button(missing completion)

--- Ticketing/ProfileCVC.swift
+++ Ticketing/ProfileCVC.swift
@@ -8,15 +8,30 @@
 
 import UIKit
 
-class ProfileCVC: UIViewController{
+class ProfileCVC: UIViewController {
     
+    //Reference to text fields
     @IBOutlet weak var fNameTextField: UITextField!
     @IBOutlet weak var lNameTextField: UITextField!
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var pwTextField: UITextField!
+    @IBOutlet weak var errorFNameLable: UILabel!
+    @IBOutlet weak var errorLNameLable: UILabel!
+    @IBOutlet weak var errorEmailLable: UILabel!
+    @IBOutlet weak var errorPWLable: UILabel!
+    @IBOutlet weak var saveButton: UIButton!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //Error messages for incorrext textField input
+        errorFNameLable.isHidden = true
+        errorLNameLable.isHidden = true
+        errorEmailLable.isHidden = true
+        errorPWLable.isHidden = true
+        
+        saveButton.layer.cornerRadius = 25.0
         
         fNameTextField.addTarget(self, action: #selector(txtFieldChanged), for: .editingDidEnd)
         lNameTextField.addTarget(self, action: #selector(txtFieldChanged), for: .editingDidEnd)
@@ -25,21 +40,96 @@ class ProfileCVC: UIViewController{
         pwTextField.isSecureTextEntry = true
     }
     
-    //Validate name fields
-    func isNameValid(name: String) -> Bool {
-        guard name.count < 3, name.count > 15 else {return}
-        
-        
-        return
-    }
-    
     @objc func txtFieldChanged(_ sender: UITextField) {
-        guard let input =  sender.text else { return }
-        
-        print("\(input)")   // <-- ERASE
+        guard let input = sender.text else { return }
     }
    
+    //Save all the updates to the profile
     @IBAction func saveButton(_ sender: UIButton) {
-        
+        if(fNameTextField.txtFieldsValidation(type: .name) == false || lNameTextField.txtFieldsValidation(type: .name) == false) {
+            if(fNameTextField.txtFieldsValidation(type: .name) == false) {
+                errorFNameLable.isHidden = false
+            }
+            if(lNameTextField.txtFieldsValidation(type: .name) == false) {
+                errorLNameLable.isHidden = false
+            }
+        }
+        else {
+            errorFNameLable.isHidden = true
+            errorLNameLable.isHidden = true
+            FirebaseAPI.shared.updateName(fNameTextField.text, lNameTextField.text) {
+                
+            }
+        }
+        if(emailTextField.txtFieldsValidation(type: .email) == false) {
+            errorEmailLable.isHidden = false
+        }
+        else {
+            errorEmailLable.isHidden = true
+            FirebaseAPI.shared.updateEmail(emailTextField.text) {
+                
+            }
+        }
+        if(pwTextField.txtFieldsValidation(type: .password) == false) {
+            errorPWLable.isHidden = false
+        }
+        else {
+            errorPWLable.isHidden = true
+            FirebaseAPI.shared.updatePassword(pwTextField.text) {
+                
+            }
+        }
+    }
+}
+
+//MARK: Extension
+extension UITextField {
+    enum txtFieldValidate {
+        case name;
+        case email;
+        case password;
+    }
+    
+    func txtFieldsValidation(type: txtFieldValidate)->Bool {
+        switch txtFieldValidate {
+        case txtFieldValidate.name:
+            return nameValidator();
+        case txtFieldValidate.email:
+            return emailValidation()
+        case txtFieldValidate.password:
+            return passwordValidation();
+        }
+    }
+    
+    private func nameValidator()->Bool {
+        if let text = text {
+            if(text.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines).isEmpty) {
+                return false
+            }
+            else if(text.count > 16) {
+                return false
+            }
+            else {
+                return true
+            }
+        }
+    }
+    
+    private func emailValidation()->Bool {
+        if let text = text, text.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines).isEmpty {
+                return false
+        }
+        else {
+            return true
+        }
+    }
+    
+    private func passwordValidation()->Bool {
+        if let text = text, text.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines).isEmpty {
+                return false
+        }
+        else {
+            return true
+        }
     }
 }
